f88902b085200a5bd1d3943a74001b192083eeb0
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2025-10-29 22:33:25 +0530

    bug fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://real-time-chating-83oz.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
   withCredentials: true,
 });
