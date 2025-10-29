cb02e4b740130134c70dd42eb6c72c5d7038e5fb
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2025-10-29 13:48:59 +0530

    some change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chating-backend.anubhavsingh.website/api" : "https://chating-backend.anubhavsingh.website/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://real-time-chating-83oz.onrender.com/api" : "https://real-time-chating-83oz.onrender.com/api",
   withCredentials: true,
 });
