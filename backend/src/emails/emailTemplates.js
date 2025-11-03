e78c032de5df879c68493732de346c356b3c6ffb
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2025-11-04 00:41:16 +0530

    emprove email template

--- backend/src/emails/emailTemplates.js
+++ backend/src/emails/emailTemplates.js
@@ -9,7 +9,7 @@ export function createWelcomeEmailTemplate(name, clientURL) {
   </head>
   <body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; line-height: 1.6; color: #333; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f5f5f5;">
     <div style="background: linear-gradient(to right, #36D1DC, #5B86E5); padding: 30px; text-align: center; border-radius: 12px 12px 0 0;">
-      <img src="https://real-time-chating.anubhavsingh.website/icon.png" alt="Website Logo" style="width: 80px; height: 80px; margin-bottom: 20px; border-radius: 50%; background-color: white; padding: 10px;">
+      <img src="https://real-time-chatting.anubhav.sbs/icon.png" alt="Website Logo" style="width: 80px; height: 80px; margin-bottom: 20px; border-radius: 50%; background-color: white; padding: 10px;">
       <h1 style="color: white; margin: 0; font-size: 28px; font-weight: 500;">Welcome to Real time Chating!</h1>
     </div>
     <div style="background-color: #ffffff; padding: 35px; border-radius: 0 0 12px 12px; box-shadow: 0 4px 15px rgba(0,0,0,0.05);">
