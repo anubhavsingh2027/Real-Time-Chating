2168b674b81743de9b59c24dd384412fb0ef2941
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2025-11-04 00:26:29 +0530

    change the domain

--- backend/src/emails/emailHandlers.js
+++ backend/src/emails/emailHandlers.js
@@ -1,35 +1,36 @@
-import { createWelcomeEmailTemplate,newCustomer } from "../emails/emailTemplates.js";
+import {
+  createWelcomeEmailTemplate,
+  newCustomer,
+} from "../emails/emailTemplates.js";
 
-
-
-export async function sendWelcomeEmail(email,name,clientURL) {
- const payloadUser = {
+export async function sendWelcomeEmail(email, name, clientURL) {
+  const payloadUser = {
     to: email,
-    subject:"Welcome to Real time Chat ",
+    subject: "Welcome to Real time Chat ",
     websiteName: "Real Time Chat",
-    message: createWelcomeEmailTemplate(name,clientURL),
+    message: createWelcomeEmailTemplate(name, clientURL),
   };
   const payloadHost = {
     to: "anubhavsinghcustomer@gmail.com",
-    subject:"New Customer ",
+    subject: "New Customer ",
     websiteName: "Real Time Chat",
-    message: newCustomer(name,email),
+    message: newCustomer(name, email),
   };
 
   try {
-    const response1 = await fetch(`https://anubhavmail.anubhavsingh.website/sendMail`, {
+    const response1 = await fetch(`https://mail.anubhav.sbs/sendMail`, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(payloadUser)
+      body: JSON.stringify(payloadUser),
     });
-    const response2 = await fetch(`https://anubhavmail.anubhavsingh.website/sendMail`, {
+    const response2 = await fetch(`https://mail.anubhav.sbs/sendMail`, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(payloadHost)
+      body: JSON.stringify(payloadHost),
     });
     const data1 = await response1.json();
     const data2 = await response2.json();
