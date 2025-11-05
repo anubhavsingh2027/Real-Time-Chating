11d87c0b83c53c2848f133ded9e3e9457dab20ff
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2025-11-05 23:43:16 +0530

    fix cors

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -12,14 +12,14 @@ export const generateToken = (userId, res) => {
   });
 
    res.cookie("jwt", token, {
-    httpOnly: true, // secure from JS access
-    secure: NODE_ENV === "development" ? false : true, // only send over HTTPS
-    sameSite: NODE_ENV === "development" ? "lax" : "none", // ✅ allow cross-domain cookies
-    path: "/", // ensures cookie is valid for all routes
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-  });
-  return token;
-};
+  httpOnly: true,
+  secure: true,          // ALWAYS TRUE since koyeb = HTTPS
+  sameSite: "None",      // ALWAYS NONE because frontend & backend = different domains
+  path: "/",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
+return token;
+
 
 // http://localhost
 // https://dsmakmk.com
