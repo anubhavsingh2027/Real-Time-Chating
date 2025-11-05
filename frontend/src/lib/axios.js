8a575c4b41d574df4e8070e0cf06594f2704dca8
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2025-11-05 13:37:03 +0530

    fix api call

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "https://chatting.backend.anubhav.sbs/api"
-      : "https://chatting.backend.anubhav.sbs/api",
+      ? "https://chatting.anubhav.sbs/api"
+      : "https://chatting.anubhav.sbs/api",
   withCredentials: true,
 });
